ea2ac84e1801fc4f7148369c01319449d7778749
Kata <katalin.molnar.kiss@gmail.com>
2017-07-06 09:22:15 +0200

    printing is done, export fails

--- printing.py
+++ printing.py
@@ -1,27 +1,28 @@
 
 # Printing functions
+from pprint import pprint
 import reports
-#importing the definitions of all replies (1-5)
+#importing the definitions of reports
 
 #1 How many games are in the file?
-print ("In this file there is " + \
-    str(reports.count_games("game_stat.txt")) + " games")
+pprint ("In this file there is " + \
+str(reports.count_games("game_stat.txt")) + " games")
 
 
 #2 Is there a game from a given year (year is an int)?
-print ("There were a game in the given year: " + \
+pprint ("There were a game in the given year: " + \
     str(reports.decide('game_stat.txt', 1998)))
 
 
 #3 Which was the latest game?
-print ("The latest game was: " + reports.get_latest('game_stat.txt'))
+pprint ("The latest game was: " + reports.get_latest('game_stat.txt'))
 
 
 #4 How many games do we have by genre?
-print ("The answer is: " + \
+pprint ("The answer is: " + \
     str(reports.count_by_genre('game_stat.txt', 'Real-time strategy')))
 
 
 #5 What is the line number of the given game (by title)?
-print ("The asked game is in line " + \
+pprint ("The asked game is in line " + \
     str(reports.get_line_number_by_title('game_stat.txt', 'Half-Life')))
