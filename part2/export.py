6df85962764d674058280702a61734e0816daea6
Kata <katalin.molnar.kiss@gmail.com>
2017-07-08 15:54:53 +0200

    question6 also exported :)

--- part2/export.py
+++ part2/export.py
@@ -1,5 +1,6 @@
 # Export functions
 import reports
+import printing
 reply1 = ('The most played game is: '+ reports.get_most_played('game_stat.txt'))
 reply2 = ('The total number of sold copies: ' + \
     str(reports.sum_sold('game_stat.txt')) + ' million')
@@ -16,7 +17,7 @@ reply5 = (str(reports.get_date_avg('game_stat.txt')) + \
 reply6 = ('The Minecraft game has the following properties: ' + \
     str(reports.get_game('game_stat.txt', 'Minecraft')))
 
-replies = [reply1, reply2, reply3, reply4, reply5]
+replies = [reply1, reply2, reply3, reply4, reply5, reply6]
 
 with open ('export.txt', 'w') as txt:
     txt.write('\n'.join(replies))
